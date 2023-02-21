136059f4a8b32ffc44cdfdd3beaea62a5e2964e1
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2023-02-21 17:02:18 +0700

    enhancement(todo): update permission code

--- routes/todo.route.go
+++ routes/todo.route.go
@@ -8,9 +8,9 @@ import (
 func TodoRoute(routes RouteGroup, controller controllers.TodoController) {
 	todo := routes.V1.Group("/todos")
 	todo.Use(middlewares.AuthCheck())
-	todo.GET("", controller.Index, middlewares.RoleCheck("TODO_MANAGEMENT", middlewares.Read))
-	todo.GET("/:id", controller.Show, middlewares.RoleCheck("TODO_MANAGEMENT", middlewares.Read))
-	todo.POST("", controller.Store, middlewares.RoleCheck("TODO_MANAGEMENT", middlewares.Create))
-	todo.PATCH("/:id", controller.Update, middlewares.RoleCheck("TODO_MANAGEMENT", middlewares.Update))
-	todo.DELETE("/:id", controller.Delete, middlewares.RoleCheck("TODO_MANAGEMENT", middlewares.Delete))
+	todo.GET("", controller.Index, middlewares.RoleCheck("TODO", middlewares.Read))
+	todo.GET("/:id", controller.Show, middlewares.RoleCheck("TODO", middlewares.Read))
+	todo.POST("", controller.Store, middlewares.RoleCheck("TODO", middlewares.Create))
+	todo.PATCH("/:id", controller.Update, middlewares.RoleCheck("TODO", middlewares.Update))
+	todo.DELETE("/:id", controller.Delete, middlewares.RoleCheck("TODO", middlewares.Delete))
 }
