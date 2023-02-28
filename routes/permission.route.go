c2ecfd874c16148ec1b1e092c3003396666a3d9d
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2023-02-28 16:52:34 +0700

    enhancement(permission): update permission

--- routes/permission.route.go
+++ routes/permission.route.go
@@ -8,6 +8,6 @@ import (
 func PermissionRoute(routes RouteGroup, controller controllers.PermissionController) {
 	permission := routes.V1.Group("/permissions")
 	permission.Use(middlewares.AuthCheck())
-	permission.GET("", controller.Index, middlewares.RoleCheck("ROLE_MANAGEMENT", middlewares.Create))
-	permission.PATCH("/assign/:roleId", controller.AssignPermissions, middlewares.RoleCheck("ROLE_MANAGEMENT", middlewares.Create))
+	permission.GET("", controller.Index, middlewares.RoleCheck("ROLE_MANAGEMENT", middlewares.Update))
+	permission.PATCH("/assign/:roleId", controller.AssignPermissions, middlewares.RoleCheck("ROLE_MANAGEMENT", middlewares.Update))
 }
